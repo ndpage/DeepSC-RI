6df90cfe0f74d67ace7558778ac23393445f3b2a
Nathan Page <npage@clemson.edu>
2025-11-25 13:04:12 -0500

    Currently single image inference using DeepSC_RI model. Inference performance (i.e. image reconstruction) is not poor. Need to improve model and/or training.

--- inference_deepsc_ri.py
+++ inference_deepsc_ri.py
@@ -2,42 +2,24 @@ import os
 import sys
 import pandas as pd
 import torch
+import torch.nn as nn
 import tqdm
-
-from traffic_light_dataset import TrafficLightDataset
-from models.deepsc_ri_classifier import build_deepsc_ri
-from torchvision import transforms
+from traffic_light_dataset import ReducedImageTrafficLightDataset
+from models.deepsc_ri import build_deepsc_ri
 import matplotlib.pyplot as plt
 import numpy as np
+from utils.devices import get_device
+from utils.visualize import to_numpy_image
+from torchvision.transforms import Resize
 
-def to_numpy_image(t: torch.Tensor):
-    # assume shape [C,H,W]; detach and move to cpu
-    if t.dim() == 4:  # [B,C,H,W] -> take first
-        t = t[0]
-    arr = t.detach().cpu().numpy()
-    if arr.shape[0] in (1,3):
-        arr = np.transpose(arr, (1,2,0))  # C,H,W -> H,W,C
-    # Normalize for display if needed
-    if arr.dtype != np.uint8:
-        arr_min, arr_max = arr.min(), arr.max()
-        if arr_max > arr_min:
-            arr = (arr - arr_min) / (arr_max - arr_min)
-    return arr
-
-
-def get_device():
-    if getattr(torch.backends, "mps", None) is not None and torch.backends.mps.is_available():
-        return torch.device("mps")
-    if torch.cuda.is_available():
-        return torch.device("cuda:0")
-    return torch.device("cpu")
 
 def inference(model_path: str, data_root: str, annotation_csv: str, snr_dB: float, fading: str, channel_dim: int):
     device = get_device()
     print("Using device:", device)
-
-    dataset = TrafficLightDataset(root=data_root, annotation_csv=annotation_csv)
-    model = build_deepsc_ri(num_classes=3, channel_dim=channel_dim, pretrained=False)
+    size = (960, 1280)
+    reduced = (size[0]//2, size[1]//2)
+    dataset = ReducedImageTrafficLightDataset(root=data_root, annotation_csv=annotation_csv, size=reduced)
+    model = build_deepsc_ri(img_size=reduced, patch_size=16)
     model.set_channel(snr_dB=snr_dB, fading=fading)
     model.load_state_dict(torch.load(model_path, map_location=device))
     model.to(device)
@@ -47,40 +29,58 @@ def inference(model_path: str, data_root: str, annotation_csv: str, snr_dB: floa
     total = 0
 
     ground_truths = []
-    predictions = []
+    reconstructions = []
     with torch.no_grad():
-        pbar: TrafficLightDataset = tqdm.tqdm(dataset, desc="Inference", leave=False)
-        for img, label in pbar:
-            img = img.unsqueeze(0).to(device)  # Add batch dimension
-            label = torch.tensor([label]).to(device)
+        pbar: ReducedImageTrafficLightDataset = tqdm.tqdm(dataset, desc="Inference", leave=False)
+        for img, orig_img in pbar:
+            img = img.to(device)
+            orig_img = orig_img.to(device)
 
-            logits, intermediates = model(img)
-            pred = logits.argmax(dim=1)
-            correct += (pred == label).sum().item()
-            total += label.size(0)
+            recnst_img, intermediates = model(img)
+            # For reconstruction accuracy, we can use MSE between orig_img and recnst_img
+            mse_loss = torch.mean((recnst_img - orig_img) ** 2, dim=[1,2,3])
+            # recon_error = (mse_loss < 0.01).long()  # threshold for reconstruction success
+            # label = torch.tensor([1 if torch.mean((orig_img - img) ** 2) < 0.01 else 0]).to(device)
 
-            ground_truths.append(label.cpu().item())
-            predictions.append(pred.cpu().item())
+            # correct += (pred == label).sum().item()
+            # total += label.size(0)
 
-    accuracy = correct / total
-    accuracy = accuracy * 100.0
+            ground_truths.append(orig_img.cpu().item())
+            reconstructions.append(recnst_img.cpu().item())
 
-    return accuracy, ground_truths, predictions, intermediates
+    # accuracy = correct / total
+    # accuracy = accuracy * 100.0
+    accuracy = 0.0  # Placeholder as reconstruction accuracy is not computed here
 
-def inference_single_image(model, image_tensor: torch.Tensor, device: torch.device):
+    return accuracy, ground_truths, reconstructions, intermediates
+
+def inference_single_image(args):
+    # Perform inference on a single sample and visualize
+    size = (960, 1280)
+    reduced = (size[0]//2, size[1]//2)
+    ds = ReducedImageTrafficLightDataset(root=args.data_root, annotation_csv=args.annotations, size=reduced)
+    image_tensor, original_img_tensor = ds[0]
+    print(f"Sample: {image_tensor.shape}, Original: {original_img_tensor.shape}")
+    
+    model = build_deepsc_ri(img_size=reduced, patch_size=16)
+    device = get_device()
+    
+    model.load_state_dict(torch.load(args.model_path, map_location=device))
+    model.to(device)
     model.eval()
-    image_tensor = image_tensor.unsqueeze(0).to(device)  # Add batch dimension
+    image_tensor = image_tensor.unsqueeze(0).to(device)
+    original_img_tensor = original_img_tensor.unsqueeze(0).to(device)
     with torch.no_grad():
-        logits, intermediates = model(image_tensor)
-        pred = logits.argmax(dim=1)
-        pred_value = pred.cpu().item()
-    return pred_value, intermediates
+        rec_image, inter = model(image_tensor)
+        upscaled_size = (original_img_tensor.shape[2], original_img_tensor.shape[3])
+        rec_image = nn.functional.interpolate(rec_image, size=upscaled_size, mode='bilinear', align_corners=False)
+    return rec_image, inter
 
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description="Inference with DeepSC-RI on Traffic Light Dataset")
-    parser.add_argument('model_path', default="deepsc_ri_traffic_light.pth" , help='Path to the trained model file')
+    parser.add_argument('model_path', default="deepsc_ri.pth" , help='Path to the trained model file')
     parser.add_argument('-d','--data-root',default="lisa-traffic-light-dataset",help='Root directory of the dataset images')
     parser.add_argument('-a','--annotations', default="lisa-traffic-light-dataset/Annotations/Annotations/daySequence1/frameAnnotationsBOX.csv", help='Path to the annotations CSV file')
     parser.add_argument('--snr', type=float, default=10.0, help='SNR in dB for the channel model')
@@ -88,68 +88,57 @@ if __name__ == "__main__":
     parser.add_argument('--fading', choices=['awgn', 'rayleigh'], default='awgn', help='Channel fading model')
     args = parser.parse_args()
 
-    acc, gt, preds, inter = inference(
-        model_path=args.model_path,
-        data_root=args.data_root,
-        annotation_csv=args.annotations,
-        snr_dB=args.snr,
-        fading=args.fading,
-        channel_dim=args.channel_dim
-    )
+    # acc, gt, preds, inter = inference(
+    #     model_path=args.model_path,
+    #     data_root=args.data_root,
+    #     annotation_csv=args.annotations,
+    #     snr_dB=args.snr,
+    #     fading=args.fading,
+    #     channel_dim=args.channel_dim
+    # )
     
-    print(f"Inference Accuracy: {acc:.4f}%")
+    # print(f"Inference Accuracy: {acc:.4f}%")
 
-    # Perform inference on a single sample and visualize
-    ds = TrafficLightDataset(root=args.data_root, annotation_csv=args.annotations)
-    sample_img, label = ds[9]
-
-    pred_value, intermediates = inference_single_image(
-        model=build_deepsc_ri(num_classes=3, channel_dim=args.channel_dim, pretrained=False).to(get_device()),
-        image_tensor=sample_img,
-        device=get_device()
-    )
+    recon_img, intermediates = inference_single_image(args)
 
     input = intermediates['input']
-    logits = intermediates['logits']
-    symbols = intermediates['symbols']
-    symbols_norm = intermediates['symbols_norm']
-    transmitted = intermediates['transmitted']
-    rec_feats = intermediates['rec_feats']
-
-    print(f"Logits: {logits.shape}")
-    print(f"Symbols: {symbols.shape}")
-    print(f"Transmitted symbols: {transmitted.shape}")
-    print(f"Reconstructed features: {rec_feats.shape}")
-
-    print(f"Single Image Prediction: {pred_value}, Ground Truth: {label}")
+    feats_seq = intermediates['feats_seq']
+    tx_norm = intermediates['tx_norm']
+    transmitted = intermediates['rx_symbols']
+    dec_symbols = intermediates['dec_output']
 
     fig = plt.figure(figsize=(12,8))
     fig.suptitle(f"DeepSC-RI Inference - SNR: {args.snr} dB, Fading: {args.fading.upper()}", fontsize=16, fontweight='bold')
     gs = fig.add_gridspec(2,2)
     ax1 = fig.add_subplot(gs[0,0])
     
-    sample_img: torch.Tensor = input.cpu()
-    sample_img_np = to_numpy_image(sample_img)
+    original_img: torch.Tensor = input.cpu()
 
-    ax1.imshow(sample_img_np)
+    # resize_transform = Resize((960, 1280)) # Specify desired output size
+    # resized_tensor = resize_transform(original_img)
+    # resized_img = to_numpy_image(recon_img)
+
+    reconstructed = to_numpy_image(recon_img)
+    original_img = to_numpy_image(original_img)
+
+    ax1.imshow(original_img)
     ax1.set_title('Image for Inference')
     ax1.margins(x=0, y=0.1)
 
     ax2 = fig.add_subplot(gs[0,1])
-    ax2.plot(symbols.cpu().numpy()[0], marker='o', linestyle='-', label='Original Symbols')
-    ax2.plot(symbols_norm.cpu().numpy()[0], marker='x', linestyle='--', label='Normalized Symbols')
+    ax2.plot(feats_seq.cpu().numpy()[0], marker='o', linestyle='-', label='Original Symbols')
+    ax2.plot(tx_norm.cpu().numpy()[0], marker='x', linestyle='--', label='Normalized Symbols')
     ax2.plot(transmitted.cpu().numpy()[0], marker='.', linestyle=':', label='Transmitted Symbols')
     ax2.set_title('Channel Symbols')
     ax2.legend(['Original Symbols', 'Normalized Symbols', 'Transmitted Symbols'], fontsize=8)
     ax2.margins(x=0, y=0.05)
 
-    probs = torch.softmax(logits, dim=1)
     ax3 = fig.add_subplot(gs[1,0])
-    ax3.bar(np.arange(probs.shape[1]), probs.cpu().numpy()[0])
-    ax3.set_title('logits Distribution')
+    ax3.imshow(reconstructed)
+    ax3.set_title('Reconstructed Image')
 
     ax4 = fig.add_subplot(gs[1,1])
-    ax4.plot(rec_feats.cpu().numpy()[0])
+    ax4.plot(dec_symbols.cpu().numpy()[0])
     ax4.set_title('Reconstructed Features')
 
     plt.show()
